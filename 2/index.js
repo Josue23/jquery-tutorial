0aaaf10538c6d30031fa52a1b6c959f7877b9f41
josue23 <josuerodrigues@gmail.com>
2016-10-29 19:51:20 -0200

    adiciodao várias funções de fade

--- 2/index.js
+++ 2/index.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-	$('#green').fadeOut(1000);
+	$('#notready').fadeOut(1000);
 });
+
+$(document).ready(function() {
+	$('button').mouseenter(function() {
+		$('button').fadeTo('slow', 1);
+	});
+	$('button').mouseleave(function() {
+		$('button').fadeTo('fast', .5);
+	});
+});
+
+
